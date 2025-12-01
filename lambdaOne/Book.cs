183ad02b3ef6d9efa36d72831796e857ef00e1b8
Kemoy Campbell <kemoycampbell45@gmail.com>
2025-12-01 15:00:11 -0500

    add get/set to the book

--- lambdaOne/Book.cs
+++ lambdaOne/Book.cs
@@ -1,4 +1,13 @@
 public class Book
 {
+    //get only
     private string Name => "Book Name";
+
+    //get set
+    private int version;
+    public int Version
+    {
+        set=> version = value;
+        get=> version;
+    }
 }
